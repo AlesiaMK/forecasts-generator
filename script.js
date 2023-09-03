85aabd380d8d62a0d2822536c92a883dfeaef215
Alesya <alesiamokretsova@gmail.com>
2023-09-03 19:32:03 +0300

    дописала код

--- script.js
+++ script.js
@@ -13,12 +13,42 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+const currentForecast = document.querySelector('.current-forecast')
+const forecasts = document.querySelector('.forecasts');
+const button = document.querySelector('.forecast-btn');
+const cardTemplate = document.querySelector('#forecast-item');
+
 function generate(min, max) {
-    let num = Math.random() * (max - min) + min;
-    return Math.round(num);
+    return Math.round(Math.random() * (max - min)) + min;
+
 };
 
-const button = document.querySelector('.forecast-btn');
-const forecasts = document.querySelector('.forecasts');
+function generatePrediction() {
+    switch (generate(1, 4)) {
+        case 1:
+            return "Ты сразу будешь понимать JS";
+        case 2:
+            return "Ты будешь есть и не толстеть";
+        case 3:
+            return "Ты отправишься в кругосветное путешествие";
+        case 4:
+            return "Тебя ждет каждый месяц прибавка к зарплате";
+    };
+};
 
-const predictionOne = 'Ты сразу начнешь понимать JS';
+button.addEventListener('click', function() {
+    let prd = generatePrediction();
+    currentForecast.querySelector('h1').textContent = prd;
+    let prc = `Вероятность: ${generate(0, 100)} %`;
+    currentForecast.querySelector('p').textContent = prc;
+
+    function makeNewForecast(title, value) {
+        const newPrediction = cardTemplate.content.cloneNode(true);
+        newPrediction.querySelector('h3').textContent = title;
+        newPrediction.querySelector('p').textContent = value;
+        return newPrediction
+    };
+    const newCard = makeNewForecast(prd, prc);
+    forecasts.prepend(newCard)
+
+})
